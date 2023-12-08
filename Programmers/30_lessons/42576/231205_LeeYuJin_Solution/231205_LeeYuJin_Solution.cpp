53aa6ee63bbc359d0d64048477c8c691d6b468ff
LeeYuJ1n <152352879+LeeYuJ1n@users.noreply.github.com>
2023-12-08 10:26:18 +0900

    Add files via upload

---
+++ Programmers/30_lessons/42576/231205_LeeYuJin_Solution/231205_LeeYuJin_Solution.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion)
+{
+    string answer = "";
+
+    map<string, int> list;
+
+    for (int i = 0; i < participant.size(); ++i)
+    {
+        if (list.find(participant[i]) != list.end())
+        {
+            list[participant[i]]++;
+        }
+        else
+        {
+            list[participant[i]] = 1;
+        }
+    }
+
+    for (int i = 0; i < completion.size(); ++i)
+    {
+        if (list.find(completion[i]) != list.end())
+        {
+            list[completion[i]]--;
+        }
+    }
+
+    for (map<string, int>::iterator it = list.begin();
+        it != list.end();
+        ++it)
+    {
+        if (it->second != 0)
+        {
+            answer = it->first;
+        }
+    }
+
+
+    return answer;
+};
